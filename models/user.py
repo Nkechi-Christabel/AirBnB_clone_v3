6bcc90915f9897accf5d7a1b3e76c01191df47df
Bamiwo Adebayo <bammietop@gmail.com>
2024-02-03 15:22:01 +0100

    Hashing Password

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ holds class User"""
 import models
+import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -27,3 +28,8 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        self.password = self._hash_password(kwargs.get('password', ''))
+
+    def _hash_password(self, password):
+        """Hashes the password using MD5"""
+        return hashlib.md5(password.encode()).hexdigest()
