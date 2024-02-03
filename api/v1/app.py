4660705f334b71f30f3d6739610ff90d21c6e3ce
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-02-03 04:06:09 -0800

    Added an HTTP access control (CORS).

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,10 +13,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     """Handle 404 errors by returning a JSON-formatted response."""
     return jsonify({"error": "Not found"}), 404
+
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
